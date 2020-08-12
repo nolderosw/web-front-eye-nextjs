eeeae138236bf9a4fef24902d3754bdc8f51c17f
Wesley Azevedo <wesley150@MacBook-Pro-de-Wesley.local>
2020-08-12 20:14:38 -0300

    modificacoes na pagina de mapa

--- pages/mapa.js
+++ pages/mapa.js
@@ -1,20 +1,60 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField'
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button'
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 export default function Mapa() {
+  const classes = useStyles();
   return (
-    <Container maxWidth="sm">
-      <Box my={4} align="center">
-        <Typography variant="h4" component="h2" gutterBottom>
-          Tabela com mapa ficará aqui
+    <Container xs="12" align="center">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Box my={4} align="center">
+            <form className={classes.root} noValidate autoComplete="off">
+              <TextField text-align="center" id="standard-basic" label="Placa do carro" />
+              <TextField id="standard-basic" label="Vaga onde ele estacionará" />
+            </form>
+            <br></br>
+            <br></br>
+            <Button color="secundary" variant="contained" color="primary">
+              <Typography color="secondary">
+                Entrar
+          </Typography>
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box my={4} align="center">
+            <Typography variant="h4" component="h2" gutterBottom>
+              Tabela com mapa REAL ficará aqui
         </Typography>
-        <Link href="/"> Voltar para o início</Link>
-        <Copyright />
-      </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box my={4} align="center">
+            <Typography variant="h4" component="h2" gutterBottom>
+              Tabela com mapa PARA O SISTEMA ficará aqui
+        </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+      <Link href="/"> Voltar para o início</Link>
+      <Copyright />
     </Container>
   );
 }
